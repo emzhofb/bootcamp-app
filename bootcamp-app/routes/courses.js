5b94fea8ca1489a6cfa9804a23ef476776673350
emzhofb <ikhdamuhammad@gmail.com>
2019-02-15 11:52:42 +0700

    menambah fitur delete course

--- bootcamp-app/routes/courses.js
+++ bootcamp-app/routes/courses.js
@@ -6,7 +6,7 @@ const models = require('../models')
 // menampilkan data dari tabel course di database ke views => courses => index
 router.get('/', function(req, res, next) {
     models.Course.findAll().then(courses => {
-        // render fungsinya sama kaya redirect
+        // render fungsinya sama kaya redirect yakni pindah (bedanya redirect ngelink ke router, render ngambil dari view)
         res.render('courses/index', {courses: courses})
         // courses di passing datanya ke views => courses => index
     }).catch(err => {
@@ -26,6 +26,21 @@ router.post('/create', (req, res) => {
         res.redirect('/courses')
     }).catch(err => {
         console.log(err)
+        res.redirect('/courses')
+    })
+})
+
+router.get('/delete/:id', (req, res) => {
+    const courseId = req.params.id
+    models.Course.findOne({
+        where: {
+            id: courseId
+        }
+    }).then(courses => {
+        return courses.destroy()
+    }).then(courses => {
+        res.redirect('/courses')
+    }).catch(err => {
         res.redirect('/courses')
     })
 })
